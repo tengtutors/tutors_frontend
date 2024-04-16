87f9eeb35b494b53c7acd46c433d4763604fc3d4
DK <donnykurniawan3707@gmail.com>
2024-04-16 18:41:14 +0700

    small

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Page = async () => {
         <div className="h-dvh flex items-center justify-center pt-20 bg-dot-white/[0.2]">
 
             {/* Desktop: Radial gradient for the container to give a faded look */}
-            <div className="hidden md:flex absolute pointer-events-none inset-0 items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+            <div className="hidden md:flex absolute pointer-events-none inset-0 items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
             <div className="max-w-[55rem] w-full flex flex-col items-center gap-5 px-5">
                 <h1 className='text-center font-extrabold text-4xl md:leading-snug md:text-5xl leading-snug'>All In One <span className='bg-primary text-transparent bg-clip-text bg-gradient-to-r from-primary via-purple-500 to-secondary animate-gradient'>AI Marketing Automation</span></h1>
